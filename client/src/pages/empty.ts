7b7859c7fcde3f6044655ef824f80c58119e520c
GOLICK-PC\Golic <Golick@tut.by>
2026-04-07 18:49:03 +0300

    fixes

--- client/src/pages/empty.ts
+++ client/src/pages/empty.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'empty',
   imports: [],
-  template: '<p>Empty page</p>',
+  template: '<p>Please login or register</p>',
 })
 export class EmptyPage
 {
